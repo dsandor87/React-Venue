53645dae1176b7ff5230604be103452bc86381e5
Sandor Desi <d.alex.galaxy@gmail.com>
2020-05-06 09:28:55 +0100

    Highlights part

--- src/App.js
+++ src/App.js
@@ -4,13 +4,23 @@ import Header from './components/header_footer/Header'
 
 import Featured from './components/featured/Featured'
 
+import VenueNfo from './components/venueNfo/VenueNfo'
+import Highlights from './components/Highlights/Highlights'
+import Pricing from './components/Pricing/Pricing'
+
 
 
 function App() {
   return (
-    <div className="App" style={{height:'1500px',background:'blue'}}>
+    <div className="App" style={{height:'1500px',background:'cornflowerblue'}}>
+          {/* <div className="App" style={{height:'1500px',background:'cornflowerblue'}}> */}
+
     <Header/>
     <Featured/>
+    <VenueNfo/>
+    <Highlights/>
+    <Pricing/>
+
     </div>
   );
 }
