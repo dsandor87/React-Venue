29c5f1e53b00436b002d0f95a9b3382ba05abc61
Sandor Desi <d.alex.galaxy@gmail.com>
2020-06-07 21:03:04 +0100

    Finished Project

--- src/components/header_footer/Header.js
+++ src/components/header_footer/Header.js
@@ -58,7 +58,7 @@ class Header extends Component {
        <ToolBar>
 
          <div className="header_logo">
-           <div className="font_righteous header_logo_venue">The Venue</div>
+           <div className="font_righteous header_logo_venue">Venue</div>
            <div className="header_logo_title">Musical Events</div>
 
          </div>
