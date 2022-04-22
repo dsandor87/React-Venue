5b66a832dd20429ea0b2a8c8758cbe16eb7096d1
Sandor Desi <d.alex.galaxy@gmail.com>
2022-04-22 14:33:15 +0200

    Updating the project

--- src/components/featured/Carrousel.js
+++ src/components/featured/Carrousel.js
@@ -3,6 +3,8 @@ import Slider from "react-slick";
 import slide_one from '../../resources/images/slide_one.jpg';
 import slide_two from '../../resources/images/slide_two.jpg';
 import slide_three from '../../resources/images/slide_three.jpg'
+import slide_four from '../../resources/images/slide_four.jpg'
+
 
 
 
@@ -29,7 +31,8 @@ const Carrousel = () => {
               <div>
                 <div className="carrousel_image"
                 style={{background:`url(${slide_one})`,      height:`${window.innerHeight}px`,
-              }}></div> 
+              }}>
+                </div> 
               </div>
               <div>
               <div className="carrousel_image"
@@ -38,6 +41,11 @@ const Carrousel = () => {
               </div> <div>
               <div className="carrousel_image"
                 style={{background:`url(${slide_three})`,      height:`${window.innerHeight}px`,
+              }}></div>
+              </div>
+              <div>
+              <div className="carrousel_image"
+                style={{background:`url(${slide_four})`,      height:`${window.innerHeight}px`,
               }}></div>
               </div>
 
